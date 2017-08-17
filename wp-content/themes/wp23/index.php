51504987ad5c235bf6147fec1b38fbe4c9445809
Mike Shepard <sheps1632@gmail.com>
2017-08-17 15:05:35 -0500

    setup php header and footer files

--- wp-content/themes/wp23/index.php
+++ wp-content/themes/wp23/index.php
@@ -1,15 +1,4 @@
-<!doctype html>
-<html class="no-js" lang="">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>wp23</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <link rel="stylesheet" href="/wp-content/themes/wp23/style.css">
-    </head>
-    <body>
+<?php get_header(); ?>
         <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
@@ -27,5 +16,4 @@
             r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
             ga('create','UA-XXXXX-X','auto');ga('send','pageview');
         </script> -->
-    </body>
-</html>
+<?php get_footer(); ?>
